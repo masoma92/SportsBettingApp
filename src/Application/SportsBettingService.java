e74235bb299da8bb55a66c622d26c2e38a8b88d1
somamakai <soma.makai@gmail.com>
2019-09-28 11:58:23 +0200

    modifies sbs class

--- src/Application/SportsBettingService.java
+++ src/Application/SportsBettingService.java
@@ -33,47 +33,6 @@ public class SportsBettingService {
     }
 
     public List<SportEvent> findAllSportEvents(){
-        /*LocalDateTime matchStartDate = LocalDateTime.of(2019, Month.OCTOBER, 02, 18, 55, 00);
-        LocalDateTime matchEndDate = LocalDateTime.of(2019, Month.OCTOBER, 02, 20, 40, 00);
-
-        LocalDateTime oddStartDate1 = LocalDateTime.of(2019, Month.OCTOBER, 02, 18, 55, 00);
-        LocalDateTime oddEndDate1 = LocalDateTime.of(2019, Month.OCTOBER, 02, 19, 10, 00);
-
-        LocalDateTime oddStartDate2 = LocalDateTime.of(2019, Month.OCTOBER, 02, 19, 11, 00);
-        LocalDateTime oddEndDate2 = LocalDateTime.of(2019, Month.OCTOBER, 02, 20, 00, 00);
-
-        LocalDateTime oddStartDate3 = LocalDateTime.of(2019, Month.OCTOBER, 02, 20, 01, 00);
-        LocalDateTime oddEndDate3 = LocalDateTime.of(2019, Month.OCTOBER, 02, 20, 40, 00);
-
-        OutcomeOdd ocob = new OutcomeOdd(new BigDecimal(1.5), oddStartDate1, oddEndDate1);
-        OutcomeOdd ocob2 = new OutcomeOdd(new BigDecimal(1.2), oddStartDate1, oddEndDate1);
-        OutcomeOdd ocob3 = new OutcomeOdd(new BigDecimal(1.05), oddStartDate1, oddEndDate1);
-        List<OutcomeOdd> outcomeOddsDortmundWin = new ArrayList<OutcomeOdd>();
-        outcomeOddsDortmundWin.add(ocob);
-        outcomeOddsDortmundWin.add(ocob2);
-        outcomeOddsDortmundWin.add(ocob3);
-
-        OutcomeOdd ocob4 = new OutcomeOdd(new BigDecimal(4), matchEndDate, matchEndDate);
-        List<OutcomeOdd> outcomeOddsSalviaWin = new ArrayList<>();
-        outcomeOddsSalviaWin.add(ocob4);
-
-        BetBuilder betb = new BetBuilder();
-        betb = betb.setDescription("winner").setType(BetType.WINNER);
-
-        Outcome o = new Outcome("Borussia Dortmund", betb.getBet(), outcomeOddsDortmundWin);
-        Outcome o2 = new Outcome("Salvia Praha", betb.getBet(), outcomeOddsSalviaWin);
-
-        betb.addOutcome(o);
-        betb.addOutcome(o2);
-
-        List<Bet> bets = new ArrayList<>();
-        bets.add(betb.getBet());
-
-        FootballSportEvent fse = new FootballSportEvent("Salvia Praha vs Borussia Dortmund", matchStartDate, matchEndDate, bets);
-
-        this.builder.addEvent(fse);
-
-        return this.builder.getEvents();*/
 
         LocalDateTime matchStartDate = LocalDateTime.of(2019, Month.OCTOBER, 02, 18, 55, 00);
         LocalDateTime matchEndDate = LocalDateTime.of(2019, Month.OCTOBER, 02, 20, 40, 00);
