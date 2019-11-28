aed513b68001e53ad3834c78735932aa7f2fe134
Soma Makai <soma.makai@gmail.com>
2019-11-28 16:51:12 +0100

    fix

--- sportsbetting-application-web/src/main/java/com/example/sportsbetting/web/configuration/WebInitializer.java
+++ sportsbetting-application-web/src/main/java/com/example/sportsbetting/web/configuration/WebInitializer.java
@@ -1,11 +1,7 @@
 package com.example.sportsbetting.web.configuration;
 
-import com.example.sportsbetting.app.AppSpring;
-import com.example.sportsbetting.app.data.DataBuilder;
 import com.example.sportsbetting.config.AppConfig;
 import com.example.sportsbetting.config.JpaConfig;
-import com.example.sportsbetting.domain.SportEvent;
-import com.example.sportsbetting.repository.SportEventRepository;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -13,7 +9,6 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import java.util.List;
 
 public class WebInitializer implements WebApplicationInitializer {
 
